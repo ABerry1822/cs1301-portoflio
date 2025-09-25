62fbd4388d7c6d9fe8312553924cd8f022d42b3d
ABerry1822 <aidan.berry0503@gmail.com>
2025-09-25 19:09:41 -0400

    Update 1_Portfolio.py

--- my_streamlit_app/pages/1_Portfolio.py
+++ my_streamlit_app/pages/1_Portfolio.py
@@ -55,7 +55,8 @@ def experience_section(experience_data):
     st.header("Professional Experience")
     for job_title, (job_description, image) in experience_data.items():
         expander = st.expander(f"{job_title}")
-        expander.image(image, width=250)
+        image_path = get_image_path(View recent photo.jpeg)
+        expander.(image, width=250)
         for bullet in job_description:
             expander.write(bullet)
     st.write("---")
