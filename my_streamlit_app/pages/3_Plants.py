f3183798ed658e1233b28a642afd632363ee823a
ABerry1822 <aidan.berry0503@gmail.com>
2025-09-25 17:42:41 -0400

    Update 3_Plants.py

--- my_streamlit_app/pages/3_Plants.py
+++ my_streamlit_app/pages/3_Plants.py
@@ -1,19 +1,20 @@
 import streamlit as st
 st.title("My Plants!")
-st.image("images/group.jpeg")
+st.image("https://imgur.com/xI24EoD")
 st.write(f'This is my collection of plants! Most of them have been with me the whole semester, but some are new')
 left, right = st.columns(2)
 with left:
     st.header(f'New Plants!')
-    st.image("Images/antherium.jpg")
+    st.image("https://imgur.com/a/C5vxOXV")
     st.write(f"This antherium plant has been back in my hometown, but now it is in Atlanta! This plant has liked a lot of light, so it enjoys my windowsill.")
-    st.image("Images/pony palm.jpg")
+    st.image("https://imgur.com/IUYhDLT")
     st.write(f'This is a new plant that my mom bought and brought to me. It is a pony palm! It is very low maintenance and only needs to be watered once a month.')
 with right:
     st.header(f'Original Plants!')
-    st.image("Images/propogation.jpg")
+    st.image("https://imgur.com/pY260fC")
     st.write(f"These two bottles are propogations of my philodendron that I brought with me! My floormates and I are going to trade propogations. Propogating philodendrons is very easy")
-    st.image("Images/buttplant.jpg")
+    st.image("https://imgur.com/undefined")
     st.write(f"Not only did this plant come with me to college, it also is my oldest plant! I am unsure what it is, but it has grown a lot with me, so I think it's cool")
-    st.image("Images/chain.jpg")
+    st.image("https://imgur.com/cNLtir5")
     st.write(f'This is a part of my group of succulents! It is not looking healthy right now, so I did not take a picture of it. When a chain of hearts blooms, it sprouts little bulbs!')
+
